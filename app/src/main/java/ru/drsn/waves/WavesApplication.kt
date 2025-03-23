a351a49a27b7fd3f7dc61eb46ef6f1a9ed3e3fbb
Dorikan <ci06122004@gmail.com>
2025-03-23 21:22:22 +0300

    Создать и зарегистрировать точку входа в приложение

---
+++ app/src/main/java/ru/drsn/waves/WavesApplication.kt
@@ -0,0 +1,11 @@
+package ru.drsn.waves
+
+import android.app.Application
+import timber.log.Timber
+
+class WavesApplication : Application() {
+    override fun onCreate() {
+
+        super.onCreate()
+    }
+}
