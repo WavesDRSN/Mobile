71d4300b9586ecd770f905ba79234ea2ea2e6bdd
Dorikan <ci06122004@gmail.com>
2025-03-23 21:24:39 +0300

    Включение логирования если приложение не в RELEASE состоянии.

--- app/src/main/java/ru/drsn/waves/WavesApplication.kt
+++ app/src/main/java/ru/drsn/waves/WavesApplication.kt
@@ -6,6 +6,11 @@ import timber.log.Timber
 class WavesApplication : Application() {
     override fun onCreate() {
 
+        // Инициализация Timber в зависимости от типа сборки
+        if (!BuildConfig.RELEASE) {
+            // В релизной версии логирование отключено
+            Timber.plant(Timber.DebugTree())
+        }
         super.onCreate()
     }
 }
