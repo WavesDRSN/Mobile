9f7c3e04417fa78eeddeb2cac3ab94196793596e
Dorikan <ci06122004@gmail.com>
2025-03-27 23:37:40 +0300

    PD-17. Реализовать сервис для gRPC соединения

--- app/src/main/java/ru/drsn/waves/signaling/SignalingServiceImpl.kt
+++ app/src/main/java/ru/drsn/waves/signaling/SignalingServiceImpl.kt
@@ -1,66 +1,68 @@
 package ru.drsn.waves.signaling
 
 import gRPC.v1.IceCandidate
-import gRPC.v1.SessionDescription
 import gRPC.v1.User
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.StateFlow
-import timber.log.Timber
-import java.io.IOException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
-class SignalingServiceImpl : SignalingService {
+class SignalingServiceImpl(
+    private var userName: String,
+    ) : SignalingService {
 
-    private var signalingConnection: SignalingConnection? = null
+    private var signalingConnection: SafeSignalingConnection? = null
+    private var usersList: List<User> = emptyList();
+    private val serviceScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
     override suspend fun connect(
         username: String,
         host: String,
         port: Int
     ) {
-        signalingConnection = SignalingConnection(host, port, username)
-        try {
-            require(username.isNotBlank()) { "Имя пользователя не может быть пустым" }
-            require(host.isNotBlank()) { "Адрес хоста не может быть пустым" }
-            require(port in 1..65535) { "Некорректный порт: $port" }
+        signalingConnection = SafeSignalingConnection(SignalingConnection(host, port, username))
+        signalingConnection!!.connect()
 
-            signalingConnection!!.connect()
-        } catch (e: IOException) {
-            throw RuntimeException("Не удалось подключиться: проблемы с сетью", e)
-            //TODO: Change RuntimeException to SignalingException
-        } catch (e: IllegalArgumentException) {
-            Timber.e(e)
-            throw RuntimeException("Bad configuration")
-            //TODO: Change RuntimeException to SignalingException
-        }
+        signalingConnection!!.observeUsersList()
+            .onEach { newList -> usersList = newList }
+            .launchIn(serviceScope)
+
     }
 
     override suspend fun disconnect() {
-        try {
-            signalingConnection!!.disconnect()
-            signalingConnection = null
-        } catch (e: IOException) {
-            throw RuntimeException("Не удалось подключиться: проблемы с сетью", e)
-            //TODO: Change RuntimeException to SignalingException
+        signalingConnection!!.disconnect()
+        signalingConnection = null
+    }
+
+    override suspend fun sendSDP(type: String, sdp: String, target: String) {
+        signalingConnection!!.sendSDP(type, sdp, target)
+    }
+
+    override fun observeSDP() {
+        serviceScope.launch {
+            signalingConnection!!.observeSDP().collect { sessionDescription ->
+                when (sessionDescription.type) {
+                    "offer" -> TODO("Логика WebRTC при получение запроса")
+                    "answer" -> TODO("Логика WebRTC при получение ответа")
+                }
+            }
+        }
+    }
+    override suspend fun sendIceCandidates(candidates: List<IceCandidate>, target: String) {
+        signalingConnection!!.sendIceCandidates(candidates, target)
+    }
+
+    override fun observeIceCandidates() {
+        CoroutineScope(Dispatchers.IO).launch {
+            signalingConnection!!.observeIceCandidates().collect { message ->
+                if (message.receiver == userName) {
+                    TODO("WebRTC add candidate to message.sender")
+                }
+            }
         }
     }
 
-    override suspend fun sendSDP(sessionDescription: SessionDescription) {
-        TODO("Not yet implemented")
-    }
-
-    override fun observeSDP(): Flow<SessionDescription> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun sendIceCandidates(candidates: List<IceCandidate>) {
-        TODO("Not yet implemented")
-    }
-
-    override fun observeIceCandidates(): Flow<List<IceCandidate>> {
-        TODO("Not yet implemented")
-    }
-
-    override fun getUsersList(): StateFlow<List<User>> {
-        TODO("Not yet implemented")
-    }
+    override fun getUsersList(): List<User> = usersList
 }
