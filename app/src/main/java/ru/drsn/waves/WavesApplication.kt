e7f0a637c55b3f94f35a03cb7f4e8385cf3c6a22
Dorikan <ci06122004@gmail.com>
2025-03-28 21:19:21 +0300

    PD-17. Добавить инициализацию сигнального сервиса

--- app/src/main/java/ru/drsn/waves/WavesApplication.kt
+++ app/src/main/java/ru/drsn/waves/WavesApplication.kt
@@ -1,9 +1,14 @@
 package ru.drsn.waves
 
 import android.app.Application
+import ru.drsn.waves.signaling.SignalingServiceImpl
 import timber.log.Timber
 
 class WavesApplication : Application() {
+
+    lateinit var signalingService: SignalingServiceImpl
+        private set
+
     override fun onCreate() {
 
         // Инициализация Timber в зависимости от типа сборки
@@ -11,6 +16,9 @@ class WavesApplication : Application() {
             // В релизной версии логирование отключено
             Timber.plant(Timber.DebugTree())
         }
+
         super.onCreate()
+
+        signalingService = SignalingServiceImpl()
     }
 }
