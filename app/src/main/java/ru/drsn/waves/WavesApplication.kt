4e28a679395a84cfb2f014e63d75a6319aca4087
Timofey <timgurov@yandex.ru>
2025-06-01 21:01:11 +0300

    PD-34 Изменить WavesApplication
    Инициализировать каналы уведомлений при старте приложения

--- app/src/main/java/ru/drsn/waves/WavesApplication.kt
+++ app/src/main/java/ru/drsn/waves/WavesApplication.kt
@@ -3,6 +3,7 @@ package ru.drsn.waves
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import org.bouncycastle.jce.provider.BouncyCastleProvider
+import ru.drsn.waves.ui.utils.NotificationHelper
 import timber.log.Timber
 import java.security.Security
 
@@ -20,6 +21,13 @@ class WavesApplication : Application() {
 
         Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME);
         Security.addProvider(BouncyCastleProvider())
+
+        try {
+            NotificationHelper.createNotificationChannels(applicationContext)
+            Timber.i("Notification channels creation process initiated.")
+        } catch (e: Exception) {
+            Timber.e(e, "Failed to create notification channels.")
+        }
 
     }
 }
