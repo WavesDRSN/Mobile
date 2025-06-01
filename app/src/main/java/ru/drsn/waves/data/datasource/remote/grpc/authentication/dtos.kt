84289d0db4d70a5f97552c71a7655ea21a7cabc5
Timofey <timgurov@yandex.ru>
2025-06-01 14:10:55 +0300

    PD-34 Обновлен dtos.kt и auth.proto
    Изменения согласно auth.proto, добавлено обновление fcm токена и изменен ответ на авторизацию

--- app/src/main/java/ru/drsn/waves/data/datasource/remote/grpc/authentication/dtos.kt
+++ app/src/main/java/ru/drsn/waves/data/datasource/remote/grpc/authentication/dtos.kt
@@ -5,4 +5,5 @@ import com.google.protobuf.ByteString
 data class NicknameReservationDto(val reservationToken: String, val expiresAtUnix: Long, val nickname: String)
 data class RegistrationResponseDto(val success: Boolean, val errorMessage: String?)
 data class ChallengeResponseDto(val challenge: ByteString, val challengeId: String)
-data class AuthenticationResponseDto(val success: Boolean, val errorMessage: String?, val jwtToken: String?)
+data class AuthenticationResponseDto(val success: Boolean, val errorMessage: String?, val jwtToken: String?, val userId: String?)
+data class UpdateTokenResponseDto(val success: Boolean, val errorMessage: String?)
